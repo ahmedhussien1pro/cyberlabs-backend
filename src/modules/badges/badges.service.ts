c1d3789ca5582961b313fbc017c76fccafd16f1e
Eng-Ahmed-Hussien <Ahmed.hussien685@el-eng.menofia.edu.eg>
2026-03-07 01:49:11 +0200

    >

--- src/modules/badges/badges.service.ts
+++ src/modules/badges/badges.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { PrismaService } from '../../../core/database';
+import { PrismaService } from '../../core/database';
 
 @Injectable()
 export class BadgesService {
