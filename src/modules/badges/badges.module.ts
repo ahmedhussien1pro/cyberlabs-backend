b727396a48c4a2ce2a746b216e9fe20d9a85f344
Eng Ahmed Hussien <ahmedhussien13520@gmail.com>
2026-03-07 03:35:22 +0200

    fix(badges): import NotificationsModule to resolve BadgesService DI crash on startup

--- src/modules/badges/badges.module.ts
+++ src/modules/badges/badges.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BadgesController } from './badges.controller';
 import { BadgesService } from './badges.service';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+  imports: [NotificationsModule], // ✅ required: BadgesService injects NotificationsService
   controllers: [BadgesController],
   providers: [BadgesService],
-  exports: [BadgesService], // exported so PracticeLabsService can inject it
+  exports: [BadgesService], // exported so PracticeLabsService / CoursesService can inject it
 })
 export class BadgesModule {}
