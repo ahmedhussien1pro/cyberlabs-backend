8c7a909e58e76e8d40acf865097ed8b99cc38a32
Eng-Ahmed-Hussien <Ahmed.hussien685@el-eng.menofia.edu.eg>
2026-03-17 01:43:47 +0200

    .

--- src/modules/practice-labs/wireshark/wireshark.module.ts
+++ src/modules/practice-labs/wireshark/wireshark.module.ts
@@ -1,14 +1,14 @@
 // src/modules/practice-labs/wireshark/wireshark.module.ts
 import { Module } from '@nestjs/common';
-import { PrismaModule } from '../../../core/database';
-import { PracticeLabSharedModule } from '../shared/practice-lab-shared.module';
+import { DatabaseModule } from '../../../core/database';
+import { SharedModule } from '../shared/shared.module';
 import { Lab1Controller } from './labs/lab1/lab1.controller';
 import { Lab1Service } from './labs/lab1/lab1.service';
 import { Lab2Controller } from './labs/lab2/lab2.controller';
 import { Lab2Service } from './labs/lab2/lab2.service';
 
 @Module({
-  imports: [PrismaModule, PracticeLabSharedModule],
+  imports: [DatabaseModule, SharedModule],
   controllers: [Lab1Controller, Lab2Controller],
   providers: [Lab1Service, Lab2Service],
 })
