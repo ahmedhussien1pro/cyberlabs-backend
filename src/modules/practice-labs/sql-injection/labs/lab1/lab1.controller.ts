bc1f1af73efb26558de0a43674b818afb7b2a5b2
Amhed Hussien <142429942+Eng-Ahmed-Hussien@users.noreply.github.com>
2026-03-16 20:11:44 +0200

    feat(sqli-lab1): add /progress endpoint

--- src/modules/practice-labs/sql-injection/labs/lab1/lab1.controller.ts
+++ src/modules/practice-labs/sql-injection/labs/lab1/lab1.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Post, Get, Body, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../../../../../common/guards';
 import { GetUser } from '../../../shared/decorators/get-user.decorator';
 import { Lab1Service } from './lab1.service';
@@ -8,11 +8,22 @@ import { Lab1Service } from './lab1.service';
 export class Lab1Controller {
   constructor(private lab1Service: Lab1Service) {}
 
+  // POST /practice-labs/sql-injection/lab1/start
   @Post('start')
   async startLab(@GetUser('id') userId: string, @Body('labId') labId: string) {
     return this.lab1Service.initLab(userId, labId);
   }
 
+  // GET /practice-labs/sql-injection/lab1/progress?labId=xxx
+  @Get('progress')
+  async getProgress(
+    @GetUser('id') userId: string,
+    @Body('labId') labId: string,
+  ) {
+    return this.lab1Service.getProgress(userId, labId);
+  }
+
+  // POST /practice-labs/sql-injection/lab1/login
   @Post('login')
   async login(
     @GetUser('id') userId: string,
